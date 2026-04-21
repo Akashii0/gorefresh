f5cc53e8588fa01efc261c904e7350fdc1aa7dea
Ojo Joel Ademola <o.joeade@gmail.com>
2026-04-21 12:57:09 +0100

    aboutus

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import logo from "../assets/logo.png";
+import left_green from "../assets/left_green.svg";
+import right_green from "../assets/right_green.svg";
+import about_go from "../assets/about_go.svg";
+import background from '../assets/background.svg';
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
+    <section  className="relative w-full min-h-screen font-geist flex flex-col bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${background})` }}>
       <Navbar logo={logo} />
+      <div className='flex gap-5 p-10 justify-center items-center text-center '>
+        <img src={left_green} alt="" />
+        <p className='text-3xl text-[#0C850C] font-bold'>About us</p>
+        <img src={right_green} alt="" />
+
+      </div>
+      <div className='flex flex-col p-5 md:grid grid-cols-2 gap-10 '>
+      <div className='flex border-black p-1 border-2  mx-20 justify-center items-center text-center gap-10'>
+        <img className='w-70 p-1' src={about_go} alt="" />
+      </div>
+      <div className='flex flex-col justify-center items-center '>
+        <div className='flex flex-col gap-5 items-center justify-center mb-10  text-[#707070]'>
+        <p>Gorefresh foods is a digital fresh food marketplace that connects producers directly to customers, enabling the purchase and delivery of organically processed food items with real-time order and delivery tracking. </p>
+      <p>Gorefresh foods is a digital fresh food marketplace that connects producers directly to customers, enabling the purchase and delivery of organically processed food items with real-time order and delivery tracking. </p>
+     
+      </div>
+         <Link to="/"  className="btn-green w-40 h-10 items-center text-center justify-center md:self-start">Back</Link>
+      </div>
       
-    </div>
+      
+      </div>
+    </section>
   )
 }
 
